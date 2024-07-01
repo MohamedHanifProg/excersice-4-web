25bfc65927121bd7f7d2df029b070b6646e6952a
MohamedHanifProg <signutre.31@gmail.com>
2024-07-01 15:24:12 +0300

    updated files for render

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-module.exports = router;
+const { register, login } = require('../controllers/authController');
 
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
